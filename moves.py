cdb4403e604f70c8f9fed523b817854c0eb02587
Nikita Buyanov <nikita.buyanov@mynbps.org>
2022-02-17 12:18:42 -0500

    updated work

--- moves.py
+++ moves.py
@@ -1,6 +1,12 @@
+from hashlib import new
 from pickle import TRUE
 
-
+def converstion (Text1,Text2):
+    newt= Text1.strip('[]') 
+    newt2 = Text2.strip('[]')
+    print (newt,newt2)
+converstion (['3','4'],['4','5'])
+    
 def chekingpawns(chessboard, x, y, x1, y1):
     if chessboard [x1] [y1] ==0:
         if x==x1 and y+1==y1:
@@ -39,6 +45,7 @@ def checkingknights (chessboard, x, y, x2, y2):
         else: 
             return False
 def checkingbishops (chessboard, x, y, x3, y3):
+    return True
 
     
 
@@ -103,6 +110,23 @@ def checkingrooks (chessboard, x, y, x4, y4):
         else: 
             return False 
         
-
-
-
+def checkingking (chessboard, x, y, x6, y6):
+    if chessboard [x6][y6] == 0:
+        if x+1==x6 and y+1==y6:
+            return True
+        elif x+2==x6 and y+1==y6:
+            return True
+        elif x+3==x6 and y+1==y6:
+            return True      
+        elif x+1==x6 and y==y6:
+            return True 
+        elif x+3==x6 and y==y6:
+            return True
+        elif x+1==x6 and y-1==y6:
+            return True
+        elif x+2==x6 and y-1==y6:
+            return True
+        elif x+3==x6 and y-1==y6:
+            return True
+        else: 
+            return False 
